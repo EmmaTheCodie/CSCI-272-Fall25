22f482af260bc1d0a7be280e4ff953d99ab90195
EmmaTheCodie <dasemma63@gmail.com>
2025-10-01 17:07:38 -0400

    Add files via upload

---
+++ Practice/Cat.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+using namespace std;
+
+class Cats {
+    public:
+        string name {"Panny"};
+            int age{1};
+            char gender{'F'};
+            string intact {"spayed"};  
+            
+            
+    public:
+        void introduce() {
+            
+     cout << "My name is " << name << ". I'm " << age << " years old." << endl << " I'm " << intact << " " << intact;
+          
+            
+           
+        }
+    };
+
+
+
+
+
+
+
+
+
+int main()
+{
+   
+}
